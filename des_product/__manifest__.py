92b89fce9a5255feb761e25ecdbed499a46181a2
MarcoMoraLite <85359179+MarcoMoraLite@users.noreply.github.com>
2022-08-11 18:59:10 -0500

    app de des adicional

---
+++ des_product/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Fengo',
+    'version': '1.0.0.0',
+    'description': 'Para mas info',
+    'author': 'Multiplicalite',
+    'data': [
+        'views/de_product.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
