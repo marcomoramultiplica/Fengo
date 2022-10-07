1f3c456a3d16923e9a891e45885883c415b27b0b
MarcoMoraLite <85359179+MarcoMoraLite@users.noreply.github.com>
2022-10-07 16:12:05 -0500

    App de prod alter

---
+++ toh_custom_website/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Personalización sitio web',
+    'version': '1.0',
+    'description': 'Ajustes en vistas del portal del sitio web',
+    'summary': 'Ajustes en vistas del portal del módul del sitio web',
+    'author': 'Toh Soluciones Digitales',
+    'website': 'http://www.tohsoluciones.com',
+    'category': 'Website/Website',
+    'depends': [
+        'website'
+    ],
+    'data': [
+        'views/templates.xml'
+    ],
+    'application': True,
+    'installable': True,
+    'license': 'OPL-1',
+}
