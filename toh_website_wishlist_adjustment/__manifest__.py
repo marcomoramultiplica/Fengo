3a764bb47249309fed80d3dd3904e8f5c2eed0ef
INOUT2022 <109757390+INOUT2022@users.noreply.github.com>
2022-12-12 14:30:41 -0600

    Add files via upload

---
+++ toh_website_wishlist_adjustment/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Ajuste diseño en Wishlist',
+    'version': '1.0',
+    'description': 'Ajuste en vista de lista de deseos del sitio web',
+    'summary': 'Ajustes de diseño en lista de deseos del módulo de sitio web',
+    'author': 'Toh Soluciones Digitales',
+    'website': 'http://www.tohsoluciones.com',
+    'category': 'Website/Website',
+    'depends': [
+        'website'
+    ],
+    'data': [
+        'views/templates.xml'
+    ],
+    'application': True,
+    'installable': True,
+    'license': 'OPL-1',
+}
